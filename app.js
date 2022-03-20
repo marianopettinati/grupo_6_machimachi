f3f118f9c4fb2f7790b86bdaeb56b63af7fdb21e
Fernando Veliz <fernando.e.veliz@gmail.com>
2022-03-20 17:18:22 -0300

    Primera configuración de carpetas, servidor y creación de la vista home

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-//probanding//
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicPath = path.resolve(__dirname, './public');
+
+app.use(express.static(publicPath));
+
+app.listen(3000, ()=>{
+    console.log("Servidor corriendo ok");
+});
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, './views/home.html'));
+})
