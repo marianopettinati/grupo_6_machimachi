2a72d7af8f88e9d58c5696c9c7edac470e342de2
marianopettinati <marianopettinati@gmail.com>
2022-04-29 12:57:54 -0300

    página de formulaio de carg de producto ruteada a user/admin/new - sin corregir css

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const productController = require ('../controllers/productController');
 
 router.get ('/', productController.product);
-
 router.get ('/:id', productController.viewProduct);
 
 module.exports = router;
