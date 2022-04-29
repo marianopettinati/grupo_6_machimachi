990f3ba8a4d082f153b1988c84fa68bf305b5489
Fernando Veliz <fernando.e.veliz@gmail.com>
2022-04-29 11:11:42 -0300

    Fix de products

--- routes/product.js
+++ routes/product.js
@@ -5,6 +5,6 @@ const productController = require ('../controllers/productController');
 
 router.get ('/', productController.product);
 
-router.get ('/:id', productController.viewProduct)
+router.get ('/:id', productController.viewProduct);
 
 module.exports = router;
