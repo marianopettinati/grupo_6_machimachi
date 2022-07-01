198df9a67c75d7d73ca6f22e60106635212b4afc
marianopettinati <marianopettinati@gmail.com>
2022-07-01 09:35:51 -0300

    actualización de herramientas de admin

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,162 +1,190 @@
-const { validationResult } = require('express-validator');
-const bcrypjs = require('bcryptjs');
-let db = require('../database/models');
+const { validationResult } = require("express-validator");
+const bcrypjs = require("bcryptjs");
+let db = require("../database/models");
+const { localsName } = require("ejs");
 
-const viewLogin = (req,res) => {
-    res.render ('login');
+const viewLogin = (req, res) => {
+  res.render("login");
 };
 
 const login = (req, res) => {
-    let errors = validationResult(req);
-    let loggedUser;
-    if (errors.isEmpty()) {
-        db.User.findOne({
-            where:{
-                email: req.body.email
-            }
-        })
-        .then(user => {
-            if(user!=null || user!=undefined)
-            {
-                let userToLogin = user.dataValues;
-                if (bcrypjs.compareSync(req.body.password, userToLogin.password)){
-                    loggedUser = userToLogin;
-                }
-                
-                if(loggedUser == null || loggedUser == undefined)
-                {
-                    return res.render('login', {errors: {
-                        credenciales:{
-                            msg: 'Credenciales inválidas'
-                        }}, old: req.body});
-                }
-                delete loggedUser.password;
-                req.session.loggedUser = loggedUser;
+  let errors = validationResult(req);
+  let loggedUser;
+  if (errors.isEmpty()) {
+    db.User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    }).then((user) => {
+      if (user != null || user != undefined) {
+        let userToLogin = user.dataValues;
+        if (bcrypjs.compareSync(req.body.password, userToLogin.password)) {
+          loggedUser = userToLogin;
+        }
 
-                if(req.body.rememberMe){
-                    res.cookie('dataEmail', req.body.email, { maxAge: 6000})
-                }
+        if (loggedUser == null || loggedUser == undefined) {
+          return res.render("login", {
+            errors: {
+              credenciales: {
+                msg: "Credenciales inválidas",
+              },
+            },
+            old: req.body,
+          });
+        }
+        delete loggedUser.password;
+        req.session.loggedUser = loggedUser;
 
-                res.redirect('/');
-            }
-            else {        
-                return res.render('login', {errors: {
-                    credenciales:{
-                        msg: 'Credenciales inválidas'
-                    }}, old: req.body});
-            }
-        })
-    }
+        if (req.body.rememberMe) {
+          res.cookie("dataEmail", req.body.email, { maxAge: 6000 });
+        }
 
-}
+        res.redirect("/");
+      } else {
+        return res.render("login", {
+          errors: {
+            credenciales: {
+              msg: "Credenciales inválidas",
+            },
+          },
+          old: req.body,
+        });
+      }
+    });
+  }
+};
 
-const logout = (req,res) => {
-    res.clearCookie('dataEmail');
-    req.session.destroy();
-    return res.redirect ('/');
-}
+const logout = (req, res) => {
+  res.clearCookie("dataEmail");
+  req.session.destroy();
+  return res.redirect("/");
+};
 
 const viewRegister = (req, res) => {
-    res.render('register', {});
-} 
+  res.render("register", {});
+};
 
 const createUser = (req, res) => {
-    
-    const resultValidation = validationResult(req);
-    if(resultValidation.errors.length > 0){
-        return res.render('register', {
-            errors: resultValidation.mapped(),
-            oldData: req.body
-        });
-    }
-    
-    let userInDB;
-    db.User.findOne({
-        where: {
-            email: req.body.email
-        }
-    }).then(response => userInDB=response)
-    
-    if(userInDB!=null || userInDB!=undefined){
-        return res.render('register', {
-            errors: {
-                email:{
-                    msg: 'Este email ya está registrado'
-                }
-            },
-            oldData: req.body
-        });
-    }
+  const resultValidation = validationResult(req);
+  if (resultValidation.errors.length > 0) {
+    return res.render("register", {
+      errors: resultValidation.mapped(),
+      oldData: req.body,
+    });
+  }
 
-    let user = {
-        ...req.body,
-        password: bcrypjs.hashSync(req.body.password, 10),
-        img: '/images/users/'+req.file.filename,
-        id_type_user: 2
-    }
+  let userInDB;
+  db.User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((response) => (userInDB = response));
 
-    db.User.create({
-        name: user.name,
-        last_name: user.last_name,
-        email: user.email,
-        phone: user.phone,
-        password: user.password,
-        img: user.img,
-        id_type_user: user.id_type_user
-    })
-    .then(() => {
-        return res.redirect('/user/login');
-    })    
+  if (userInDB != null || userInDB != undefined) {
+    return res.render("register", {
+      errors: {
+        email: {
+          msg: "Este email ya está registrado",
+        },
+      },
+      oldData: req.body,
+    });
+  }
+
+  let user = {
+    ...req.body,
+    password: bcrypjs.hashSync(req.body.password, 10),
+    img: "/images/users/" + req.file.filename,
+    id_type_user: 2,
+  };
+
+  db.User.create({
+    name: user.name,
+    last_name: user.last_name,
+    email: user.email,
+    phone: user.phone,
+    password: user.password,
+    img: user.img,
+    id_type_user: user.id_type_user,
+  }).then(() => {
+    return res.redirect("/user/login");
+  });
+};
+
+const viewProfile = (req, res) => {
+  res.render("userProfile", {});
+};
+
+const profilesList = (req, res) => {
+  db.User.findAll()
+    .then(function (user) {
+        res.render("userList", { user: user });
+  });
+};
+
+const userEdit = (req,res) => {
+    db.User.findAll()
+        .then (function (user) {
+            if (res.locals.loggedUser.id_type_user == 1)  {
+                res.render ("userList", {user})
+            } else {
+                res.render ("userProfile")
+            }
+    });
 }
 
-const viewProfile = (req,res) => {
-    res.render ('userProfile', {});
+const userDelete = (req,res) => {
+    res.render ("userList")
 }
 
 const viewForgotPassword = (req, res) => {
-    res.render('forgotpassword', {});
+  res.render("forgotpassword", {});
 };
 
 const updateUser = (req, res) => {
-    const resultValidation = validationResult(req);
-    if(resultValidation.errors.length > 0){
-        return res.render('register', {
-            errors: resultValidation.mapped(),
-            oldData: req.body
-        });
-    }
+  const resultValidation = validationResult(req);
+  if (resultValidation.errors.length > 0) {
+    return res.render("register", {
+      errors: resultValidation.mapped(),
+      oldData: req.body,
+    });
+  }
 
-    let user = {
-        ...req.body,
-        password: bcrypjs.hashSync(req.body.password, 10),
-        img: '/images/users/'+req.file.filename,
-        id_type_user: 2
-    }
+  let user = {
+    ...req.body,
+    password: bcrypjs.hashSync(req.body.password, 10),
+    img: "/images/users/" + req.file.filename,
+    id_type_user: 2,
+  };
 
-    db.User.update({
-        name: user.name,
-        last_name: user.last_name,
-        email: user.email,
-        phone: user.phone,
-        password: user.password,
-        img: user.img,
-        id_type_user: user.id_type_user
+  db.User.update(
+    {
+      name: user.name,
+      last_name: user.last_name,
+      email: user.email,
+      phone: user.phone,
+      password: user.password,
+      img: user.img,
+      id_type_user: user.id_type_user,
     },
-    {where: {id_user: req.params.id}})
-    .then(() => {
-        //TODO: Crear la ruta user/edit/:id para redireccionar a una vista
-    })
-}
+    { where: { id_user: req.params.id } }
+  ).then(() => {
+    //TODO: Crear la ruta user/edit/:id para redireccionar a una vista
+  });
+};
 
 const loginController = {
-    viewLogin,
-    login,
-    logout,
-    viewRegister,
-    createUser,
-    viewProfile,
-    viewForgotPassword,
-    updateUser
+  viewLogin,
+  login,
+  logout,
+  viewRegister,
+  createUser,
+  viewProfile,
+  viewForgotPassword,
+  updateUser,
+  profilesList,
+  userDelete,
+  userEdit,
 };
 
 module.exports = loginController;
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,40 +1,50 @@
-const express = require ('express');
+const express = require("express");
 const router = express.Router();
-const path = require ('path');
-const multer = require ('multer');
-const validaciones = require('../middlewares/validationUtils');
-const guestMiddleware = require ('../middlewares/guestMiddleware');
-const authMiddleware = require ('../middlewares/authMiddleware');
+const path = require("path");
+const multer = require("multer");
+const validaciones = require("../middlewares/validationUtils");
+const guestMiddleware = require("../middlewares/guestMiddleware");
+const authMiddleware = require("../middlewares/authMiddleware");
+const adminMiddleware = require("../middlewares/adminMiddleware");
 
-const storage = multer.diskStorage({ 
-    destination: function (req, file, cb) { 
-       cb(null, path.join(__dirname, '../../public/images/users')); 
-    }, 
-    filename: function (req, file, cb) { 
-       const nombreDeImg = file.fieldname + Date.now() + path.extname(file.originalname);
-       cb(null, nombreDeImg);
-    }
-  });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../../public/images/users"));
+  },
+  filename: function (req, file, cb) {
+    const nombreDeImg =
+      file.fieldname + Date.now() + path.extname(file.originalname);
+    cb(null, nombreDeImg);
+  },
+});
 
 const uploadFile = multer({ storage });
 
+const loginController = require("../controllers/userController");
 
-const loginController = require ('../controllers/userController');
+router.get("/login", guestMiddleware, loginController.viewLogin);
 
+router.post("/login", validaciones.validacionesLogin, loginController.login);
 
-router.get('/login', guestMiddleware, loginController.viewLogin);
+router.get("/register", guestMiddleware, loginController.viewRegister);
 
-router.post('/login', validaciones.validacionesLogin, loginController.login);
+router.post(
+  "/register",
+  uploadFile.single("userImg"),
+  validaciones.validacionesRegister,
+  loginController.createUser
+);
 
-router.get('/register', guestMiddleware, loginController.viewRegister);
+router.get("/forgotpassword", loginController.viewForgotPassword);
 
-router.post('/register', uploadFile.single('userImg'), validaciones.validacionesRegister, loginController.createUser);
+router.get("/profile", authMiddleware, loginController.viewProfile);
 
-router.get('/forgotpassword', loginController.viewForgotPassword);
+router.post("/logout", loginController.logout);
 
-router.get('/profile', authMiddleware, loginController.viewProfile);
-
-router.post('/logout', loginController.logout);
+//enrutadores del list
+router.get("/list", adminMiddleware, loginController.profilesList); //FALTA EL ADMINMIDDLEWARE, no lo puse para testear más fácil
+router.put ("/list", loginController.userEdit);
+router.delete ("/list", loginController.userDelete);
 
 
 module.exports = router;
